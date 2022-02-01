2fbab90a94dbceb5f1d5cbe8a2325049680274a5
winkatme <67841298+winkatme@users.noreply.github.com>
2022-02-01 10:33:27 -0500

    Update code.py

--- code.py
+++ code.py
@@ -25,11 +25,13 @@ new_image = ds.pixel_array.astype(float)
 scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255
 
 # Iterate conversion to image type, Save in folder \Images
+# Note: It appears that the images need to be manually converted to 8-bit in order for the Gaussian blur to work correctly
+# Other modes: https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
 os.makedirs('Images', exist_ok=True)
 for i in range(0,len(scaled_image[:])):
     image_number = i+1
     scaled_image_copy = scaled_image[i,:,:]
     img = Image.fromarray(scaled_image_copy)
-    img = img.convert(mode='L')   # For some reason I had to convert it to 8-bit mode L for gaussian to work (Modes: https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes)
+    img = img.convert(mode='L')
     img = img.filter(ImageFilter.GaussianBlur(radius=0.6))
     img.save(f'Images\image_{image_number}.{Image_Type}')
